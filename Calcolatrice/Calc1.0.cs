e3d50f2adb5dd342de0f044207a068ad390c9feb
michiburro14@gmail.com <michiburro14@gmail.com>
2020-11-16 21:46:48 +0100

    Add project files.

---
+++ Calcolatrice/Calc1.0.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Operazioni;
+namespace Calcolatrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, riultato = 0;
+            string operazione;
+            num1 = classoperazioni.PRIMONUMERO();
+            num2 = classoperazioni.SECONDONUMERO();
+            Console.WriteLine("inserisci la tua operazione");
+            operazione = Console.ReadLine();
+            switch (operazione)
+            {
+                case "Somma":
+                case "+":
+
+                    riultato = classoperazioni.SOMMA(num1, num2);
+                    break;
+
+
+
+                case "Sottrazione":
+                case "-":
+
+                    riultato = classoperazioni.SOTTRAZIONE(num1, num2);
+                    break;
+
+
+
+                case "Moltiplicazione":
+                case "*":
+
+                    riultato = classoperazioni.MOLTIPLICAZIONE(num1, num2);
+                    break;
+
+
+
+                case "Divisione":
+                case "/":
+
+                    riultato = classoperazioni.DIVISIONE(num1, num2);
+                    break;
+            }
+            Console.WriteLine($"risultato è {riultato}");
+        }
+    }
+}
---
+++ Operazioni/classoperazioni.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Operazioni
+{
+    public class classoperazioni
+    {
+        public static double PRIMONUMERO()
+        {
+            Console.Write("inserisci il primo numero");
+            double num1 = double.Parse(Console.ReadLine());
+            return num1;
+        }
+        public static double SECONDONUMERO()
+        {
+            Console.Write("inserisci il secondo numero");
+            double num2 = double.Parse(Console.ReadLine());
+            return num2;
+        }
+        public static double SOMMA(double num1, double num2)
+        {
+            double somma = num1 + num2;
+            return somma;
+
+
+        }
+        public static double SOTTRAZIONE(double num1, double num2)
+        {
+            double sottra = num1 + num2;
+            return sottra;
+
+
+        }
+        public static double MOLTIPLICAZIONE(double num1, double num2)
+        {
+            double molt = num1 * num2;
+            return molt;
+
+
+        }
+        public static double DIVISIONE(double num1, double num2)
+        {
+            double div = num1 / num2;
+            return div;
+
+
+        }
+
+    }
+}
